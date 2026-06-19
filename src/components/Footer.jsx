bca73809bf86647c02fc812bd11d6bd285f2e70e
DeVybrant1 <biakuvigil2@gmail.com>
2026-06-19 20:27:53 +0000

    ui bugs fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import git_logo from '../assets/github-svg.svg'
 export default function Footer(){
 
     return(
-        <div className="flex items-center flex-row justify-center p-4 bg-gray-800 text-white absolute bottom-0 w-full">
+        <div className="flex items-center flex-row justify-center p-4 bg-gray-800 text-white sticky bottom-0 w-full">
             <p>2026 Emmet Trainer. No rights reserved, yet. 😊 </p>
             <a href="https://github.com/emmett-trainer" target="_blank" rel="noopener noreferrer">
                 <img src={git_logo} className="h-6 w-6 ml-6" alt="Github Link" />
