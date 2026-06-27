1dc5121b6504ba92c5f3c53697150687f6269724
DeVybrant1 <biakuvigil2@gmail.com>
2026-06-27 23:50:13 +0000

    new branch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router"
 function Navbar() {
   return (
-    <nav className="flex items-center p-4 bg-blue-500">
+    <nav className="flex items-center p-4 bg-green-500">
       <Link to="/" className="font-bold text-2xl">Emmet Trainer</Link>
 
       <ul className="ml-auto flex gap-6">
